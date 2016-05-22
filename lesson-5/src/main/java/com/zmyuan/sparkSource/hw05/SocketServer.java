cd46e624289a6f28e3319195edebc69223dcf3f9
zhudebin <zdb_cn@163.com>
2016-05-22 17:40:52 +0800

    'lesson5'

---
+++ lesson-5/src/main/java/com/zmyuan/sparkSource/hw05/SocketServer.java
@@ -0,0 +1,107 @@
+package com.zmyuan.sparkSource.hw05;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by zdb on 2016/5/22.
+ */
+public class SocketServer {
+
+    public static void main(String args[]) {
+
+        try {
+
+            ServerSocket server = null;
+
+            try {
+
+                server = new ServerSocket(8818);
+
+                //创建一个ServerSocket在端口8818监听客户请求
+
+            } catch (Exception e) {
+
+                System.out.println("can not listen to:" + e);
+
+                //出错，打印出错信息
+
+            }
+
+            Socket socket = null;
+
+            try {
+
+                socket = server.accept();
+                System.out.println("---------连接到socket ----------");
+
+                //使用accept()阻塞等待客户请求，有客户
+
+                //请求到来则产生一个Socket对象，并继续执行
+
+            } catch (Exception e) {
+
+                System.out.println("Error." + e);
+
+                //出错，打印出错信息
+
+            }
+
+            String line;
+
+            //由Socket对象得到输入流，并构造相应的BufferedReader对象
+
+            PrintWriter os = new PrintWriter(socket.getOutputStream());
+
+            //由Socket对象得到输出流，并构造PrintWriter对象
+
+            BufferedReader sin = new BufferedReader(new InputStreamReader(System.in));
+
+            //在标准输出上打印从客户端读入的字符串
+
+            line = sin.readLine();
+            System.out.println(line);
+
+            //从标准输入读入一字符串
+
+            while (!line.equals("bye")) {
+
+                //如果该字符串为 "bye"，则停止循环
+
+                os.println(line);
+
+                //向客户端输出该字符串
+
+                os.flush();
+
+                //刷新输出流，使Client马上收到该字符串
+
+                System.out.println("Server:" + line);
+
+                //从Client读入一字符串，并打印到标准输出上
+
+                line = sin.readLine();
+
+                //从系统标准输入读入一字符串
+
+            } //继续循环
+
+            os.close(); //关闭Socket输出流
+
+            socket.close(); //关闭Socket
+
+            server.close(); //关闭ServerSocket
+
+        } catch (Exception e) {
+
+            System.out.println("Error:" + e);
+
+            //出错，打印出错信息
+
+        }
+
+    }
+}
---
+++ lesson-5/src/main/scala/com/zmyuan/sparkSource/hw05/WordCount.scala
@@ -0,0 +1,47 @@
+package com.zmyuan.sparkSource.hw05
+
+import java.io._
+
+import org.apache.spark._
+import org.apache.spark.streaming._
+/**
+  * Created by zdb on 2016/5/22.
+  */
+object WordCount {
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
+    val ssc = new StreamingContext(conf, Seconds(1))
+
+    val lines = ssc.socketTextStream("localhost", 8818)
+
+    val words = lines.flatMap(_.split(" "))
+    // Count each word in each batch
+    val pairs = words.map(word => (word, 1))
+    val wordCounts = pairs.reduceByKey(_ + _)
+
+    // Print the first ten elements of each RDD generated in this DStream to the console
+    wordCounts.filter(t2 => {
+      t2._2>5
+    }).foreachRDD((rdd, time) => {
+      val list = rdd.collect()
+      if(list.size > 0) {
+        val file = new File("wc_" + time.milliseconds + ".txt")
+        val bw = new BufferedWriter(new FileWriter(file))
+        list.foreach(t2 => {
+          val word = t2._1
+          val count = t2._2
+          bw.write(s"$word,$count")
+          bw.newLine()
+        })
+        bw.flush()
+        bw.close()
+      }
+    })
+//      .saveAsTextFiles("wc", "txt")
+
+    ssc.start()             // Start the computation
+    ssc.awaitTermination()  // Wait for the computation to terminate
+  }
+
+}
