2541a806afd6fa52d6bd1642dc836184d2b812c5
zhudebin <zdb_hn@163.com>
2016-05-28 16:30:06 +0800

    no2 no3

--- lesson-6/src/main/scala/com/zmyuan/sparkhw/hw06/WebLogAnalysis.scala
+++ lesson-6/src/main/scala/com/zmyuan/sparkhw/hw06/WebLogAnalysis.scala
@@ -27,33 +27,86 @@ object WebLogAnalysis {
 
     val lines2 = lines.filter(!_.isEmpty).map(line => {
       val strs = line.split(" ")
+      // IP, url, browers
       (strs(0), strs(6), strs(12))
     })
 
-    lines2.map(t3 => (t3._1, 1)).reduceByKey(_ + _).updateStateByKey((values:Seq[Int], state:Option[Int]) => {
-      var newSum = state.getOrElse(0)
-      values.foreach(i => {
-        newSum += i
-      })
-      Some(newSum)
-    }).foreachRDD((rdd, time) => {
-      val list = rdd.top(2)(new Ordering[(String, Int)] {
-        override def compare(x: (String, Int), y: (String, Int)): Int = {
-          val dif = x._2 - y._2
-          if(dif > 0) {
-            1
-          } else if(dif == 0) {
-            0
-          } else {
-            -1
+    def no1(): Unit = {
+      // 第一题
+      lines2.map(t3 => (t3._1, 1)).reduceByKey(_ + _).updateStateByKey((values:Seq[Int], state:Option[Int]) => {
+        var newSum = state.getOrElse(0)
+        values.foreach(i => {
+          newSum += i
+        })
+        Some(newSum)
+      }).foreachRDD((rdd, time) => {
+        val list = rdd.top(2)(new Ordering[(String, Int)] {
+          override def compare(x: (String, Int), y: (String, Int)): Int = {
+            val dif = x._2 - y._2
+            if(dif > 0) {
+              1
+            } else if(dif == 0) {
+              0
+            } else {
+              -1
+            }
           }
-        }
+        })
+        println(s"------rdd id: ${rdd.id} -----")
+        list.foreach(t2 => {
+          println(s"${t2._1} cnt:${t2._2}, time:${time}")
+        })
       })
-      println(s"------rdd id: ${rdd.id} -----")
-      list.foreach(t2 => {
-        println(s"${t2._1} cnt:${t2._2}, time:${time}")
+    }
+
+    def no2(): Unit ={
+      // 第二题
+      lines2.map(t3 => (t3._2, 1)).reduceByKey(_ + _).updateStateByKey((values:Seq[Int], state:Option[Int]) => {
+        var newSum = state.getOrElse(0)
+        values.foreach(i => {
+          newSum += i
+        })
+        Some(newSum)
+      }).foreachRDD((rdd, time) => {
+        // top
+        val list = rdd.top(50)(new Ordering[(String, Int)] {
+          override def compare(x: (String, Int), y: (String, Int)): Int = {
+            val dif = x._2 - y._2
+            if(dif > 0) {
+              1
+            } else if(dif == 0) {
+              0
+            } else {
+              -1
+            }
+          }
+        })
+        println(s"------rdd id: ${rdd.id} -----")
+        list.foreach(t2 => {
+          println(s"${t2._1} cnt:${t2._2}, time:${time}")
+        })
       })
-    })
+    }
+
+    def no3(): Unit = {
+      // 第三题
+      lines2.map(t3 => t3._3)
+        .transform((rdd) => rdd.distinct()) // 去重
+        .map(br => {
+        (br, 1)
+      }).updateStateByKey((se:Seq[Int], state:Option[String]) => {
+        Some("aa")
+      }).foreachRDD((rdd) => {
+        rdd.collect().foreach(t2 => {
+          println(s"${t2._1}")
+        })
+      })
+    }
+
+    no3()
+
+
+
 //      .saveAsTextFiles("wc", "txt")
 
     ssc.start()             // Start the computation
